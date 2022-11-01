b67536ab21d1b1a5241879aecac215dc4a13ca42
alexia2212 <a20192270@pucp.edu.pe>
2022-11-01 14:21:00 -0500

    prueba

---
+++ src/main/java/Beans/Clientes.java
@@ -0,0 +1,10 @@
+package Beans;
+
+public class Clientes {
+    private String nombreCliente;
+    private String edad;
+    private String tipoCliente;
+    private String tipoDocumento;
+    private int numeroDocumento;
+    private int cantidadDeContratos;
+}
---
+++ src/main/java/Beans/Contratos.java
@@ -0,0 +1,10 @@
+package Beans;
+
+public class Contratos {
+    private String nombreCliente;
+    private String nroDeContrato;
+    private int estadoDelContrato;
+    private int mesesEnEseEstado;
+    private String divisa;
+    private float expectedLoss;
+}
